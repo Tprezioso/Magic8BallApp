bbb9596298f54581f5402f02fa276fba92367d2a
Tprezioso <tommyprezioso@gmail.com>
2018-11-12 22:00:08 -0500

    finished app just need to add constraints to UI

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballArray = ["ball1","ball2","ball3","ball4","ball5"]
+    var randomBallNumber = 0
+    
+    
     @IBOutlet weak var eightBallImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        randomAnswer()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        randomAnswer()
     }
 
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        randomAnswer()
+    }
     
+    func randomAnswer() {
+        randomBallNumber = Int.random(in: 0...4)
+        eightBallImage.image = UIImage(named: ballArray[randomBallNumber])
     }
     
 }
